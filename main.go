876c4430dc4936fc951a190f7962deb4b588111c
Charles Guertin <charlesguertin@live.ca>
2018-10-08 22:09:25 -0400

    Beginning of the logic.

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (eb englishBot) getGreeting() string {
+	// VERY custom logic for english greeting
+	return "Hi There!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
 }
